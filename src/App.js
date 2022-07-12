a779fd0040d1408edfabb135cab2fb5d6800b832
Jmorg21 <Jmorg21@gmail.com>
2022-07-12 15:50:35 -0400

    cleaning up files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Footer from "./components/footer/footer";
-//import About from "./components/about/about"
-//import Contact from "./components/contact/contact"
 import Nav from "./components/navigation/nav";
-//import Project from "./components/project/project";
 import MainContainer from "./components/page/main";
 
 function App() {
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,8 +4,8 @@ function Footer(){
     return(
       <footer>
         <a href="https://github.com/Jmorg21">GitHub</a>
-        <a herf="mailto:Jmorg21@gmail.com">Email</a>
         <a href="https://www.linkedin.com/in/jamesmorgan21/">LinkedIn</a>
+        <a href="mailto:Jmorg21@gmail.com">Email</a>
       </footer>
     )
 } 
